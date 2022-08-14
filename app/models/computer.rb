c0876009fb5b802ef53701665474f020a0258279
potemotto <gurataso0509@gmail.com>
2022-08-14 19:50:42 +0000

    bootstrap_update2

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -4,6 +4,6 @@ class Computer < ApplicationRecord
   belongs_to :system
   belongs_to :public
 
-  enum storage_type: {"2.5インチHDD": 1,"3.5インチHDD": 2,"2.5インチSSD":3,"M.2_SSD":4,"その他":9,"不明":0}
+  enum storage_type: {"2.5インチHDD": 1,"3.5インチHDD": 2,"2.5インチSSD":3,"M.2_SSD":4,"それ以外":9,"分からない":0}
   enum pc_type: {"デスクトップPC": 0,"ノートPC": 1,"一体型":2,"その他":9,"不明":0}
 end
